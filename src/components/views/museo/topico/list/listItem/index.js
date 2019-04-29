739f466e6cfeb7bcfa31058a5db57b35d6195f32
Diego Martinez Diaz <diego.martinezdiaz@linkerx.com.ar>
2019-04-29 20:50:00 -0300

    mejoras imagenes

--- src/components/views/museo/topico/list/listItem/index.js
+++ src/components/views/museo/topico/list/listItem/index.js
@@ -7,7 +7,10 @@ var Link = require('react-router-dom').Link;
 function TopicoItem(props) {
 
   var item_image = '';
-  if(props.item._embedded && props.item._embedded['wp:featuredmedia']){
+
+  if(props.item.imagenes_dest && props.item.imagenes_dest.length > 1){
+    item_image = props.item.imagenes_dest[1].full;
+  } else if(props.item._embedded && props.item._embedded['wp:featuredmedia']){
     if(props.item._embedded['wp:featuredmedia'][0].media_details.sizes['medium']){
       item_image = props.item._embedded['wp:featuredmedia'][0].media_details.sizes['medium'].source_url;
     }
